2540923eafb188001139a0e9c7f9836daf75df9f
mayedatariq <mayedatariq24@gmail.com>
2026-06-20 00:17:49 +0500

    Add Again Twenty Five! [A] from Codeforces

---
+++ 630/A - Again Twenty Five!/solution.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    long long int x;
+    cin >> x;
+    cout << 25;
+    
+    return 0;
+}
